9e3443a42a6f6a1d5c3ad172924e016b217d5856
Cloves José <clovesjsilva8@gmail.com>
2022-08-21 23:22:01 -0300

    travis config

---
+++ ponto/ponto/src/main/java/br/com/projetoPonto/ponto/PontoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoPonto.ponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoApplication.class, args);
+	}
+
+}
---
+++ ponto/ponto/src/test/java/br/com/projetoPonto/ponto/PontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoPonto.ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
