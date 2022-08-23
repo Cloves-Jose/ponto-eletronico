bbb2ec0efec57ba9da1cc460f91fea9cdf5ed94e
Cloves José <clovesjsilva8@gmail.com>
2022-08-23 19:45:03 -0300

    FuncionarioRepository

--- backend/src/main/java/br/com/projetoPonto/ponto/repositories/FuncionarioRepository.java
+++ backend/src/main/java/br/com/projetoPonto/ponto/repositories/FuncionarioRepository.java
@@ -1,5 +1,14 @@
 package br.com.projetoPonto.ponto.repositories;
 
-public interface FuncionarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.projetoPonto.ponto.entity.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+	
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	
+	Funcionario findByCpfOrEmail(String cpf, String email);
 }
