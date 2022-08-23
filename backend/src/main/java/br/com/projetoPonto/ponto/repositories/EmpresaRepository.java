b42effee57a1f6399b621978aa845854b8a41bcc
Cloves José <clovesjsilva8@gmail.com>
2022-08-23 19:43:08 -0300

    EmpresaRepository

--- backend/src/main/java/br/com/projetoPonto/ponto/repositories/EmpresaRepository.java
+++ backend/src/main/java/br/com/projetoPonto/ponto/repositories/EmpresaRepository.java
@@ -1,5 +1,13 @@
 package br.com.projetoPonto.ponto.repositories;
 
-public interface EmpresaRepository {
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.projetoPonto.ponto.entity.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+	
+	@Transactional
+	Empresa findByCnpj(String cnpj);
 }
