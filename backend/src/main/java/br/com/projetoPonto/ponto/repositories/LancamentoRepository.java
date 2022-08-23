583c7c599b6b6c4a8876149b0fbd18798470797a
Cloves José <clovesjsilva8@gmail.com>
2022-08-23 20:35:43 -0300

    lancamentoRepositori

--- backend/src/main/java/br/com/projetoPonto/ponto/repositories/LancamentoRepository.java
+++ backend/src/main/java/br/com/projetoPonto/ponto/repositories/LancamentoRepository.java
@@ -1,12 +1,13 @@
 package br.com.projetoPonto.ponto.repositories;
 
-import java.awt.print.Pageable;
+
 import java.util.List;
 
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
