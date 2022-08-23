df395591d6f384f224d5629d1bce73c96bc6fbad
Cloves José <clovesjsilva8@gmail.com>
2022-08-23 20:28:32 -0300

    empresa test

--- backend/src/test/java/br/com/projetoPonto/ponto/repositories/EmpresaRepositoryTest.java
+++ backend/src/test/java/br/com/projetoPonto/ponto/repositories/EmpresaRepositoryTest.java
@@ -1,5 +1,46 @@
 package br.com.projetoPonto.ponto.repositories;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.projetoPonto.ponto.entity.Empresa;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
 public class EmpresaRepositoryTest {
+	
+	@Autowired
+	private EmpresaRepository empresaRepostory;
+	
+	private static final String CNPJ = "95201293000181";
+	
+	@Before
+	public void setUp() throws Exception {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj(CNPJ);
+		this.empresaRepostory.save(empresa);
+	}
+	
+	@After
+	public final void tearDown() {
+		this.empresaRepostory.deleteAll();
+	}
+	
+	@Test
+	public void testBuscarPoCnpj() {
+		Empresa empresa = this.empresaRepostory.findByCnpj(CNPJ);
+		
+		assertEquals(CNPJ, empresa.getCnpj());
+	}
 
 }
