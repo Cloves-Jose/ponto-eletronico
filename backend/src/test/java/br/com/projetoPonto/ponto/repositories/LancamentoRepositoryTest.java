96b21016499e3d829a59b5657e588cc13f5a018d
Cloves José <clovesjsilva8@gmail.com>
2022-08-23 23:03:10 -0300

    travis.yml

--- backend/src/test/java/br/com/projetoPonto/ponto/repositories/LancamentoRepositoryTest.java
+++ backend/src/test/java/br/com/projetoPonto/ponto/repositories/LancamentoRepositoryTest.java
@@ -1,5 +1,58 @@
 package br.com.projetoPonto.ponto.repositories;
 
-public class LancamentoRepositoryTest {
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.projetoPonto.ponto.entity.Empresa;
+import br.com.projetoPonto.ponto.entity.Funcionario;
+import br.com.projetoPonto.ponto.enums.PerfilEnum;
+import br.com.projetoPonto.ponto.utils.PasswordUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class LancamentoRepositoryTest {
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepostiroy;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private static final String EMAIL = "email@email.com";
+	private static final String CPF = "63340273014";
+	
+	@Before
+	public void setUp() throws Exception {
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+	}
+	
+	
+	
+	private Funcionario obterDadosFuncionario(Empresa empresa) {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Fulano de Tal");
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
+		funcionario.setCpf(CPF);
+		funcionario.setEmail(EMAIL);
+		funcionario.setEmpresa(empresa);
+		
+		return funcionario;
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj("95201293000181");
+		return empresa;
+	}
 }
