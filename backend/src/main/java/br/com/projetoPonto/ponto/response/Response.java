16caa8b262ec778ee74e355144f39b93aec57594
Cloves José <clovesjsilva8@gmail.com>
2022-08-26 18:08:06 -0300

    PJController/PJDTO

--- backend/src/main/java/br/com/projetoPonto/ponto/response/Response.java
+++ backend/src/main/java/br/com/projetoPonto/ponto/response/Response.java
@@ -1,5 +1,40 @@
 package br.com.projetoPonto.ponto.response;
 
+import java.util.ArrayList;
+import java.util.List;
+/***
+ * 
+ * @author Clove José Duarte Silva
+ * 
+ * Está classe irá encapsular o retorno da API
+ *
+ * @param <T>
+ */
 public class Response<T> {
+	
+	private T data;
+	private List<String> errors;
+	
+	public Response() {}
 
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public List<String> getErrors() {
+		if (this.errors == null) {
+			this.errors = new ArrayList<String>();
+		}
+		return errors;
+	}
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+	
+	
 }
